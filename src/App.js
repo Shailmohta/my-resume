8d750d3df74d020a0b578edb46e61b84a9d44d0b
Shailesh Mohta <mohta.shail@gmail.com>
2023-10-10 15:29:42 +0530

    Navigaion bar fixed on top

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import Navbar from './Components/NavigationBar';
-import SideBar from './Components/SideBar';
+import Navbar from './Components/NavigationBar';
+// import SideBar from './Components/SideBar';
 import Content from './Components/Content';
 
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
-      <SideBar/>
+      <Navbar />
+      {/* <SideBar/> */}
       <Content/>
     </div>
   );
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,42 +1,60 @@
-import React from "react";
+import React from 'react'
 
-export default function Navbar(props) {
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+function SideBar() {
+  return (
+    <div style={{height:"80px"}}>
+        <nav className="navbar bg-dark navbar-expand-lg border-body p-3 fixed-top" data-bs-theme="dark" id='navBar'>
+            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="offcanvas offcanvas-start" id="offcanvasExample">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title my-2" id="offcanvasExampleLabel">Navigation</h5>
+                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mx-3 me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="#navBar">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#About">About Me</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#Experience">Work Experience</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#Education">Education</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#Contact">Contact Me</a>
+                </li>
+                <li className="nav-item dropdown" style={{width:"320px"}}>
+                    <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Skill-Set
                     </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <ul className="dropdown-menu px-3" >
+                    <li><b>Front-End Web development</b></li>
+                    <li>HTML, CSS, JavaScript</li>
+                    <li>python</li>
+                    <li><b>Back-End - Connections and Requests</b></li>
+                    <li>PHP</li>
+                    <li>DataBase Management</li>
+                    <li><b>3D Modeling Basics</b></li>
+                    <li>Creo and AutoCad</li>
+                    <li><b>Video Editing</b></li>
+                    <li>Adobe PremierPro and AfterEffects</li>
+                    <li><b>Graphic Designer</b></li>
+                    <li>Adobe Illustrator and PhotoShop</li>
                     </ul>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
+                </li>
                 </ul>
-                <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
                 </div>
             </div>
+            <h3 className="d-flex flex-row-reverse mx-3 my-1" style={{color: `white`}}>Shailesh Mohta</h3>
         </nav>
-    )
+    </div>
+  )
 }
+
+export default SideBar
